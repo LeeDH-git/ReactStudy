d7bdee8dfed396f063fdbf197bf69ef329d9ec19
LeeDH-git <djrl02@naver.com>
2021-11-08 16:42:07 +0900

    dd

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Auth from "../hoc/auth";
 // pages for this product
@@ -6,21 +6,36 @@ import LandingPage from "./views/LandingPage/LandingPage.js";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
-import Footer from "./views/Footer/Footer"
+import Footer from "./views/Footer/Footer";
+import UploadProductPage from "./views/UploadProductPage/UploadProductPage.js";
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
 //false  logged in user can't go inside
 
+/** Switch - Route로 생성된 자식컴포넌트중 매칭되는 첫번째 Route를 렌더링해줍니다. 
+    보통 메인 Route의 url경로를 "/"로 지정하고 다른 Route에는 "/detail", "/login" 이런식으로 지정하게 되는데, 
+    만약 Switch를 사용하지 않으면 그냥 메인페이지에 가기 위하여 
+    "/" 경로를 접속했늗네 "/"가 포함된 "/detail", "/login" 컴포넌트들이 다 렌더링 되는 경우를 보게 됩니다.
+    
+    exact path - 정확히 일치하는 경로
+    
+    */
+
 function App() {
   return (
-    <Suspense fallback={(<div>Loading...</div>)}>
+    <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null)} />
-          <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path='/' component={Auth(LandingPage, null)} />
+          <Route exact path='/login' component={Auth(LoginPage, false)} />
+          <Route exact path='/register' component={Auth(RegisterPage, false)} />
+          <Route
+            exact
+            path='/product/upload'
+            component={Auth(UploadProductPage, true)}
+          />
         </Switch>
       </div>
       <Footer />
