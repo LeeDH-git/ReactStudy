d0b354fd1dfc39a06426cc918407d870b7ec880e
LeeDH-git <djrl02@naver.com>
2021-11-17 16:37:10 +0900

    dd

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, Input, Select } from "antd";
-import Title from "antd/lib/skeleton/Title";
+import FileUpload from "../util/FileUpload";
 
 const { TextArea } = Input;
 
@@ -44,6 +44,9 @@ function UploadProductPage() {
       </div>
 
       <Form>
+        {/* Drop Zone  */}
+        <FileUpload />
+
         <br />
         <br />
         <label>이름</label>
